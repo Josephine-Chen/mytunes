d15fd50fd7c92b78e5027e32d28218e27e998064
Josephine Chen <jychen@g.hmc.edu>
2017-01-04 16:57:59 -0800

    Song model, queue, and queue view tests pass

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,6 +4,30 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
+    this.on('add', function() {
+      if (!this.at(1)) {
+        this.playFirst();
+      }
+    });
+
+    this.on('ended', function() {
+      this.remove(this.at(0));
+      if (this.at(0)) {
+        this.playFirst();
+      }
+    });
+
+    this.on('dequeue', function(song) {
+      var songIndex = this.indexOf(song);
+      if (songIndex !== -1) {
+        this.remove(this.at(songIndex));
+      }
+    });
+
+  },
+
+  playFirst: function() {
+    this.at(0).play();
   }
 
 });
